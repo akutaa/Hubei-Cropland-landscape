598ca86b89759af573fa15dcf23f0b0c5fc1c400
akutaa <stage9961@gmail.com>
2023-03-22 00:22:22 +0800

    testing different lsm

--- landscape.R
+++ landscape.R
@@ -5,36 +5,43 @@ library(tidyr)
 library(terra)
 library(sf)
 library(landscapemetrics)
-# inRas <-　rast("E:/Code/data/testRas.tif")
-inRas <- rast("E:/Code/data/CLCD_v01_2021_albert_hubei.tif")
+
+inRas <- rast("2021_Hubei_Re.tif")
+check_landscape(inRas,verbose = TRUE)
 plot(inRas)
 
-check_landscape(inRas,verbose = TRUE)
+# inVec <- st_read("Hubei.shp")
+# rawGrid <- st_make_grid(x = inVec,cellsize = 10000,what = "polygons",square = TRUE)
 
-# testgrid <- st_make_grid(x = inRas,cellsize = 1000,what = "polygons",square = TRUE)
-# plot(testgrid)
-# testrange <- st_sf(testgrid)
-# testgrid <- st_read("E:/Code/data/grid_1km_sp.shp")
-testgrid <- st_read("E:/Code/data/HubeiSP10km__.shp")
-plot(testgrid)
+samplePoint <- st_read("HubeiSP10km.shp")
+plot(samplePoint)
 
+# selected lsm: 
+# ai, ca, lpi, np, pland, circle_cv, frac_cv
+# issue?
 metric <- sample_lsm(inRas,
-                     y = testgrid,
-                     level = "class",
-                     metric = c("ai","shei"),
-                     plot_id = testgrid$GRID_ID,
+                     y = samplePoint,
+                     # level = "class",
+                     # metric = c("frac_cv","frac_mn","frac_sd"),
+                     what = c("lsm_c_frac_cv","lsm_c_frac_mn","lsm_c_frac_sd"),
+                     plot_id = samplePoint$GRID_ID,
                      shape = "square",
                      size = 5000,#remember to update when scale changed
                      all_classes = FALSE,
-                     return_raster = TRUE,#whether to check raster
+                     directions = 8,
+                     neighbourhood = 8,
+                     return_raster = FALSE,#whether to check raster
                      verbose = TRUE,
                      progress = TRUE)
 
 # if return raster TRUE, plot
-plot(metric$raster_sample_plots[[1]])
+# plot(metric$raster_sample_plots[[1]])
 
 metric <- pivot_wider(data = metric,names_from = metric,values_from = value)
-outGrid <- merge(x = testgrid,y = metric,by.x="GRID_ID",by.y="plot_id",all.x=TRUE)
-plot(outGrid["shei"])
+metric <- dplyr::filter(metric, class == 1)
 
+outGrid <- merge(x = samplePoint,y = metric,by.x="GRID_ID",by.y="plot_id",all.x=TRUE)
 
+plot(outGrid["frac_cv"])
+plot(outGrid)
+plot(outGrid, max.plot = 13)
