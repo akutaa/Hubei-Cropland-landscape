f9ca5de6e22dbd3897ffdfc26666a3c18dc684ed
akutaa <stage9961@gmail.com>
2023-03-24 22:53:31 +0800

    10 km done

--- landscape.R
+++ landscape.R
@@ -7,26 +7,38 @@ library(sf)
 library(landscapemetrics)
 
 inRas <- rast("2021_Hubei_Re.tif")
-check_landscape(inRas,verbose = TRUE)
+# check_landscape(inRas,verbose = TRUE)
 plot(inRas)
 
 # inVec <- st_read("Hubei.shp")
 # rawGrid <- st_make_grid(x = inVec,cellsize = 10000,what = "polygons",square = TRUE)
 
-samplePoint <- st_read("HubeiSP10km.shp")
+# samplePoint <- st_read("HubeiSP10km.shp")
+samplePoint <- st_read("HubeiSP3km.shp")
 plot(samplePoint)
 
 # selected lsm: 
-# ai, ca, lpi, np, pland, circle_cv, frac_cv
-# issue?
+# ai, ca, lpi, np, pland, circle_cv, contig_cv, frac_cv, para_mn, shape_cv, cai_cv, core_cv, dcore_cv
+# ndca, tca, area_cv, ed, gyrate_cv, te, cohesion, division, lsi, pladj
+# pafrac with multiple NA
+# issue: cannot calculate by using level and metric
 metric <- sample_lsm(inRas,
                      y = samplePoint,
-                     # level = "class",
-                     # metric = c("frac_cv","frac_mn","frac_sd"),
-                     what = c("lsm_c_frac_cv","lsm_c_frac_mn","lsm_c_frac_sd"),
+                     level = "class",
+                     # metric = c('circle', 'contig', 'frac', 'para', 'shape'),
+                     type = "shape metric",
+                     # what = c(
+                     #   "lsm_c_pafrac",
+                     #   "lsm_c_ca",
+                     #   "lsm_c_pland",
+                     #   "lsm_c_ai",
+                     #   "lsm_c_np",
+                     #   "lsm_c_split",
+                     #   "lsm_c_lpi"
+                     #   ),
                      plot_id = samplePoint$GRID_ID,
                      shape = "square",
-                     size = 5000,#remember to update when scale changed
+                     size = 1500,#!!!!!!!!remember to update when scale changed!!!!!!!!!
                      all_classes = FALSE,
                      directions = 8,
                      neighbourhood = 8,
@@ -37,11 +49,28 @@ metric <- sample_lsm(inRas,
 # if return raster TRUE, plot
 # plot(metric$raster_sample_plots[[1]])
 
-metric <- pivot_wider(data = metric,names_from = metric,values_from = value)
-metric <- dplyr::filter(metric, class == 1)
+# bkup for calc metric
+metric2 <- metric
 
-outGrid <- merge(x = samplePoint,y = metric,by.x="GRID_ID",by.y="plot_id",all.x=TRUE)
+#check duplicate value
+# metric2 %>%
+#   dplyr::group_by(layer, level, class, id, plot_id, percentage_inside, metric) %>%
+#   dplyr::summarise(n = dplyr::n(), .groups = "drop") %>%
+#   dplyr::filter(n > 1L) 
 
-plot(outGrid["frac_cv"])
+# pay attention to U-42 cell, maybe NA
+
+metric2 <- dplyr::filter(metric2, class == 1)
+metric2 <- pivot_wider(data = metric2,names_from = metric,values_from = value)
+outGrid <- merge(x = samplePoint,y = metric2,by.x="GRID_ID",by.y="plot_id",all.x=TRUE)
+
 plot(outGrid)
-plot(outGrid, max.plot = 13)
+plot(outGrid["np"])
+plot(outGrid, max.plot = 30)
+
+# corr
+corr <- calculate_correlation(metrics = metric,method = "pearson")
+show_correlation(data = corr,method = "pearson")
+
+# reset environment
+rm(metric, metric2, outGrid)
